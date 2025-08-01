043fc633ca4339a21c3bd64e935930e2e3eb56eb
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-01 23:42:21 +0600

    change some code in html and access the dom element in javascript done on 08/01/2025 at 11:42 pm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 //? qr code generator app ------------------------------------------------------------------>
+
+// access dom element ----------------------------->
+const input = document.getElementById('input');
+const qrCodeBox = document.getElementById('qr-code-box');
+const loading = document.getElementById('loading');
+const qrCode = document.getElementById('qr-code');
+const qrGenerateBtn = document.getElementById('generate-btn');
+
